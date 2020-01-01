99546fd7b697c6b11ffb85b4602a2d9f7354b1bd
anam soomro <anamsoomroed@gmail.com>
2020-01-01 19:55:31 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,17 @@ def find_max_value(array)
   array.pop
 end
 
+#def find_min_value(array)
+  #array.sort!
+  #array.shift
+#end
+
 def find_min_value(array)
-  array.sort!
-  array.shift
+  min = array[0]
+  array.length.times do |index|
+   if array[index] < min
+    min = array[index]
+   end
+  end
+  min
 end
